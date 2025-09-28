f8681170c16dbd0f1cdf212104d68d8744884481
aesadov <aesadov@gmail.com>
2025-09-28 22:08:32 +0300

    feat: footer layout for PC added

---
+++ components/AppFooter.vue
@@ -0,0 +1,115 @@
+<script setup>
+  import { ref } from 'vue'
+  import IconTwitter from '~/assets/icons/Icon-twitter.svg'
+  import IconInstagram from '~/assets/icons/Icon-Instagram.svg'
+  import IconFaceBook from '~/assets/icons/Icon-FB.svg'
+
+  import { APP_LINKS } from '~/constants/links'
+
+  const {
+    CONTACT_LINK,
+    TERMS_LINK,
+    SHIPPING_LINK,
+    PRIVACY_POLICY,
+    TWITTER_LINK,
+    INSTAGRAM_LINK,
+    FACEBOOK_LINK,
+  } = APP_LINKS
+
+  const errMessage = ref('')
+</script>
+
+<template>
+  <footer class="footer">
+    <hr class="divider" />
+    <div class="main">
+      <nav class="main__nav">
+        <NuxtLink class="main__link" :to="CONTACT_LINK">CONTACT</NuxtLink>
+        <NuxtLink class="main__link" :to="TERMS_LINK">TERMS OF SERVICES</NuxtLink>
+        <NuxtLink class="main__link" :to="SHIPPING_LINK">SHIPPING AND RETURNS</NuxtLink>
+      </nav>
+      <BaseInput :error="inputErr" :placeholder="PLACEHOLDER" />
+    </div>
+    <div class="bottom">
+      <div class="bottom__terms">
+        © 2021 Shelly.
+        <NuxtLink class="bottom__terms-link" :to="TERMS_LINK">Terms of use </NuxtLink>and
+        <NuxtLink class="bottom__terms-link" :to="PRIVACY_POLICY">privacy policy.</NuxtLink>
+      </div>
+      <div class="bottom__social">
+        <a :href="TWITTER_LINK" target="_blank"><IconTwitter class="bottom__icon" /></a>
+        <a :href="INSTAGRAM_LINK" target="_blank"><IconInstagram class="bottom__icon" /></a>
+        <a :href="FACEBOOK_LINK" target="_blank"><IconFaceBook class="bottom__icon" /></a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+    margin-top: 253px;
+  }
+
+  .divider {
+    background-color: #d8d8d8;
+  }
+
+  .main {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 37px;
+    color: $main-text-color;
+
+    &__nav {
+      display: flex;
+      gap: 41px;
+      justify-content: space-between;
+    }
+
+    &__link {
+      font-size: 16px;
+      color: inherit; /* ← Наследует цвет от родителя */
+      text-decoration: none; /* ← Убирает подчеркивание */
+      cursor: pointer;
+
+      &:hover {
+        color: #000; /* ← Цвет при наведении */
+      }
+    }
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 30px;
+
+    &__icon {
+      color: $main-text-color;
+      cursor: pointer;
+
+      &:hover {
+        color: #000;
+      }
+    }
+
+    &__social {
+      display: flex;
+      gap: 30px;
+    }
+
+    &__terms {
+      font-size: 16px;
+    }
+
+    &__terms-link {
+      color: $main-text-color;
+      cursor: pointer;
+
+      &:hover {
+        color: #000;
+      }
+    }
+  }
+</style>
---
+++ components/BaseInput.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+  import IconArrow from '~/assets/icons/Icon-arrow.svg'
+  import { ref } from 'vue'
+
+  interface Props {
+    error?: boolean
+    placeholder?: string
+    errorMessage?: string
+  }
+
+  const {
+    error = false,
+    placeholder = 'Введите текст',
+    errorMessage = 'Ошибка ввода',
+  } = defineProps<Props>()
+
+  const isChecked = ref(false)
+</script>
+
+<template>
+  <div class="input-container">
+    <div class="input-wrapper">
+      <input
+        class="input"
+        :class="{ 'input--error': error }"
+        :placeholder="error ? errorMessage : placeholder"
+      />
+      <IconArrow class="input__icon" :class="{ 'input__icon--disabled': !isChecked }" />
+    </div>
+    <div class="info">
+      <input v-model="isChecked" class="info__checkbox" type="checkbox" />
+      <span class="info__text">i agree to the website's terms and conditions</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .input-container {
+    position: relative;
+  }
+
+  .input-wrapper {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+  }
+
+  .input {
+    width: 100%;
+    padding-bottom: 13px;
+    outline: none;
+    background: transparent;
+    border: none;
+    border-bottom: 2px solid $main-text-color;
+
+    &:focus {
+      border-bottom-color: blue;
+    }
+
+    &::placeholder {
+      color: $main-text-color;
+      transition: color 0.2s ease;
+    }
+
+    &--error {
+      border-bottom-color: red;
+
+      &::placeholder {
+        color: red;
+      }
+    }
+  }
+
+  .input__icon {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    cursor: pointer;
+    fill: $main-text-color;
+    transform: translateY(-50%);
+    transition: all 0.3s ease;
+
+    &:hover {
+      fill: #000;
+    }
+
+    &--disabled {
+      pointer-events: none;
+      cursor: not-allowed;
+      fill: #ccc;
+    }
+  }
+
+  .info {
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+
+    &__checkbox {
+      margin-right: 8px;
+    }
+
+    &__text {
+      font-size: 14px;
+      color: $main-text-color;
+    }
+  }
+</style>
