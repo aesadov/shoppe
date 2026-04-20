777ed36f197bdc43dd60b7f3234637938bbb515e
aesadov <aesadov@gmail.com>
2026-04-20 15:20:28 +0300

    feat: desktop layout for SingIn & Register components finished

--- pages/account.vue
+++ pages/account.vue
@@ -1,86 +1,32 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import BaseInput from '~/components/BaseInput.vue'
-  import { useInput } from '~/composables/useInput'
-  import { fieldValidators } from '~/utils/validation'
+  const isSignInShow = ref(true)
 
-  const {
-    value: userName,
-    error: userNameError,
-    hasError: userNameHasError,
-    resetForm: resetUserName,
-  } = useInput()
-
-  const {
-    value: password,
-    error: passError,
-    hasError: passHasError,
-    resetForm: resetPass,
-  } = useInput()
-
-  const saveUserInfo = ref(false)
-
-  const validateForm = () => {
-    userNameHasError.value = false
-    passHasError.value = false
-
-    let isValid = true
-
-    const usernameValidation = fieldValidators.username(userName.value)
-    if (!usernameValidation.isValid) {
-      userNameError.value = usernameValidation.error
-      userNameHasError.value = true
-      isValid = false
-    }
-
-    const passwordValidation = fieldValidators.password(password.value)
-    if (!passwordValidation.isValid) {
-      passError.value = passwordValidation.error
-      passHasError.value = true
-      isValid = false
-    }
-
-    return isValid
-  }
-
-  const handleSubmit = () => {
-    if (validateForm()) {
-      // Логика входа
-      console.log('Login attempt', { userName: userName.value, password: password.value })
-    }
-  }
+  const signInShowHandler = () => (isSignInShow.value = !isSignInShow.value)
 </script>
 
 <template>
   <div class="account">
     <h1>My account</h1>
-    <div class="account__buttons">
-      <button>Sign in</button>
-      <button>Register</button>
-    </div>
-    <form @submit.prevent="handleSubmit">
-      <BaseInput
-        v-model="userName"
-        :placeholder="'Username'"
-        type="form"
-        :error="userNameHasError"
-        :error-message="userNameError"
-      />
-      <BaseInput
-        v-model="password"
-        :placeholder="'Password'"
-        type="form"
-        :error="passHasError"
-        :error-message="passError"
-      />
-      <label>
-        <input v-model="saveUserInfo" type="checkbox" :checked="saveUserInfo" />
-        <span>Remember me</span>
-      </label>
 
-      <button type="submit">SIGN IN</button>
-      <div>Have you forgot your password?</div>
-    </form>
+    <div class="account__buttons">
+      <button
+        class="account__btn"
+        :class="{ 'account__btn--active': isSignInShow }"
+        @click="isSignInShow = true"
+      >
+        Sign in
+      </button>
+      <button
+        class="account__btn"
+        :class="{ 'account__btn--active': !isSignInShow }"
+        @click="isSignInShow = false"
+      >
+        Register
+      </button>
+    </div>
+
+    <SignIn v-if="isSignInShow" />
+    <Register v-else />
   </div>
 </template>
 
@@ -89,5 +35,47 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    h1 {
+      margin-bottom: 60px;
+      font-size: 33px;
+      font-weight: 500;
+    }
+
+    &__buttons {
+      display: flex;
+      gap: 2px;
+      width: 100%;
+      max-width: 500px;
+      padding: 5px;
+      margin-bottom: 126px;
+      background-color: #e9e9ed;
+      border-radius: 8px;
+    }
+
+    &__btn {
+      flex: 1;
+      height: 50px;
+      font-size: 16px;
+      font-weight: 500;
+      text-align: center;
+      cursor: pointer;
+      background: transparent;
+      border: none;
+      border-radius: 6px;
+      transition: all 0.25s ease;
+
+      &:hover:not(.account__btn--active) {
+        color: #000;
+        background-color: rgb(0 0 0 / 3%);
+      }
+
+      &--active {
+        background-color: #fff;
+        box-shadow:
+          0 2px 8px rgb(0 0 0 / 5%),
+          0 1px 2px rgb(0 0 0 / 10%);
+      }
+    }
   }
 </style>
