dcbdb80740b501544b7d4a6d9b1a61829d922df4
aesadov <aesadov@gmail.com>
2026-06-26 23:30:23 +0300

    fix: add document title for accessibility and SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,7 @@ export default defineNuxtConfig({
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL || '/',
     head: {
+      title: 'Shoppe',
       link: [
         {
           rel: 'icon',
